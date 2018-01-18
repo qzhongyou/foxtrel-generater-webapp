fba226cd2e9dea5e4041799b1c8da27006378a42
阙中友 <quezhongyou@zhubajie.com>
2018-01-18 18:14:09 +0800

    测试配置信息

--- app/foxtrel.config.js
+++ app/foxtrel.config.js
@@ -1,1 +1,6 @@
-foxtrel.config.set()
+//根据项目类型,选择不同编译器进行编译
+foxtrel.config.set('type', 'react');
+
+
+foxtrel.config.set('name', 'projectName');
+foxtrel.config.set('webpack.output.path',__dirname+"/dist")
